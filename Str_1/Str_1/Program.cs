5f07f0024af23deaa8bf3fafa2a937898ce879c8
Laurenande <57595984+Laurenande@users.noreply.github.com>
2021-07-10 16:53:55 +0300

    Add files via upload

---
+++ Str_1/Str_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+namespace Str_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            StringBuilder str = new StringBuilder( Console.ReadLine());
+            for (int index = 0; index < str.Length; index++)
+            {
+                switch (str[index])
+                {
+                    case 'a':
+                        str[index] = 'b';
+                        count++;
+                        break;
+                    case 'b':
+                        str[index] = 'a';
+                        count++;
+                        break;
+                }
+            }
+            Console.WriteLine(str);
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ Str_2/Str_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+namespace Str_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Console.WriteLine("Введите строку:");
+            StringBuilder str = new StringBuilder(Console.ReadLine());
+            for (int index = 0; index < str.Length; index++)
+            {
+                switch (str[index])
+                {
+                    case 'v':
+                        str[index] = 'V';
+                        count++;
+                        break;
+                    case 'c':
+                        str[index] = 'C';
+                        count++;
+                        break;
+                }
+            }
+            Console.WriteLine(str);
+            Console.WriteLine(count);
+        }
+    }
+}
